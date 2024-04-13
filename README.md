ef8bef476f371566d03980c6f6a18e61ac2e6dcd
Davi Nascimento <galaxys3tab32@gmail.com>
2024-04-13 20:48:07 +0000

    update de codigo

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # flask-react
 Comunicação de backend flask com frontend react.
+
+- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \o/
+# API com react e flask
+
+## APLICAÇAO FUNCIONANDO 100%
+
+## aplicacao com frontend react e backend flask e banco sqlite usando o ORM peewee para criacao de tabelas
+
+## OBS: nao foi resolvido o problema de salvar no banco mysql(estava funcionando)
+
+## comunicacao do react com o flask por api que conecta a rota com  a biblioteca axios do React
+
+### porque usar O flask-cors
+
+- criar a pasta=> flask-server
+- rodar o comando => npx create-react-app client
+- pip install flask
+- pip install peewee
+- pip install flask-cors
+
+- colocar esta linha de codigo no arquivo package.json=> "proxy":"http://localhost:5000",
+
+- dentro de react-clien , instalar o axios=> npm install axios
+
+- rodar server flask
+- rodar o server react
+
+- Quando você desenvolve uma aplicação web que faz solicitações Ajax (por exemplo, usando o fetch ou XMLHttpRequest no JavaScript) de um domínio para outro, é possível que você se depare com restrições de CORS. Essas restrições podem impedir que o navegador execute a solicitação, a menos que o servidor permita explicitamente solicitações provenientes do domínio da sua aplicação.
+
+- O flask-cors simplifica o processo de habilitação do CORS em um aplicativo Flask, fornecendo uma maneira fácil de configurar quais origens (domínios) são permitidas, quais métodos HTTP são permitidos, como lidar com cabeçalhos personalizados e outras configurações relacionadas ao CORS.
---
+++ flask-server/main.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, jsonify
+from peewee import Model, MySQLDatabase, CharField, IntegerField, SqliteDatabase, DateTimeField
+from flask_cors import CORS
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+# Aplicando CORS à aplicação Flask
+CORS(app)
+
+# Configurações do banco de dados MySQL 
+# db = MySQLDatabase('storage', user='root', password='', host='localhost', port=3306)
+
+# Configurações do banco de dados SQLite
+db = SqliteDatabase('storage.sqlite')
+
+# Definindo um modelo Peewee para a tabela
+class Pessoa(Model):
+    nome = CharField()
+    idade = IntegerField()
+    email = CharField()
+    created_at = DateTimeField(default=datetime.now)  
+
+    class Meta:
+        database = db
+
+# Criar a tabela no banco de dados
+db.connect()
+db.create_tables([Pessoa], safe=True)
+
+# Rota principal que renderiza a página inicial com o formulário React
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+# Rota para processar os dados do formulário enviado pelo React
+@app.route('/adicionar_pessoa', methods=['POST'])
+def adicionar_pessoa():
+    try:
+        data = request.get_json()
+
+        nome = data.get('nome')
+        idade = data.get('idade')
+        email = data.get('email')
+
+        with db.atomic():
+            pessoa = Pessoa.create(
+                nome=nome,
+                idade=idade,
+                email=email
+            )
+        db.commit()  # Certifique-se de que a transação seja commitada
+        print(f"Pessoa {pessoa.id} adicionada com sucesso")
+        return jsonify({'mensagem': 'Pessoa adicionada com sucesso!'})
+    except Exception as e:
+        print(f"Erro ao adicionar pessoa: {str(e)}")
+        return jsonify({'erro': str(e)})
+    
+
+# Rota para obter os dados das pessoas em formato JSON
+@app.route('/pessoas')
+def obter_pessoas():
+    pessoas = Pessoa.select().order_by(Pessoa.created_at.desc())
+    pessoas_json = [{'id': pessoa.id, 'nome': pessoa.nome, 'idade': pessoa.idade, 'email': pessoa.email, 'created_at': pessoa.created_at.strftime('%Y-%m-%d %H:%M:%S')} for pessoa in pessoas]
+    return jsonify(pessoas_json)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
